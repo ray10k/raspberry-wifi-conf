e7d96077a275d7436412e354488e1a666a1f8c3a
Wouter van den Heuvel <w.j.van.den.heuvel@xmsnet.nl>
2020-04-20 11:49:17 +0200

    post-test cleanup.

--- service.js
+++ service.js
@@ -1,12 +1,10 @@
 var async               = require("async"),
     wifi_manager        = require("./app/wifi_manager")(),
-    dependency_manager  = require("./app/dependency_manager")(),
-    config              = require("./config.json");
+    dependency_manager  = require("./app/dependency_manager")()
 
 /*****************************************************************************\
     1. Check for dependencies
-    2. Disable the wifi entirely, to provide a known basic state.
-    3. Host a lightweight HTTP server which allows for the user to connect and
+    2. Host a lightweight HTTP server which allows for the user to connect and
        configure the RPIs wifi connection. The interfaces exposed are RESTy so
        other applications can similarly implement their own UIs around the
        data returned.
@@ -24,20 +22,7 @@ async.series([
             next_step(error);
         });
     },
-/*
-    // 2. Disable wifi entirely.
-    function disable_wifi(next_step) {
-        wifi_manager.shutdown_wireless_network(config.access_point.wifi_interface, function(error) {
-            if(error) {
-                console.log("... wifi disable ERROR: " + error);
-            } else {
-                console.log("... successfully disabled wifi!");
-            }
-            next_step(error);
-        });
-    },
-*/
-    // 3. Host HTTP server, the "api.js" file contains all the needed logic to
+    // 2. Host HTTP server, the "api.js" file contains all the needed logic to
     //    get a basic express server up. It uses a small angular application 
     //    which allows us to choose the wifi of our choosing.
     function start_http_server(next_step) {
