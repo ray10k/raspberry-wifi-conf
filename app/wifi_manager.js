3ccde8ae9045d6e7e8df998b00db82923afd4205
Wouter van den Heuvel <w.j.van.den.heuvel@xmsnet.nl>
2020-05-22 11:10:01 +0200

    Fix the "empty network block" issue.

--- app/wifi_manager.js
+++ app/wifi_manager.js
@@ -535,7 +535,7 @@ module.exports = function() {
             });
             //Push the remaining known SSIDs to the end of the list so they're last to
             //get connected to, and to ensure no SSIDs are accidentally forgotten.
-            new_list.push(entries);
+            new_list = new_list.concat(entries);
             _save_wpa_config(callback,new_list);
         });
     }
